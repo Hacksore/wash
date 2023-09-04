d0e0426b73e4f38fb46345b16c2f8c7548cc0305
Juan Ruiz Valencia <juan7732@gmail.com>
2023-09-04 13:14:16 -0500

    l18n support

--- wash/utils.py
+++ wash/utils.py
@@ -1,3 +1,16 @@
+import os
+import gettext
+
+LOCALE_DIR = 'locales'
+LANGUAGES = ['en_US', 'es_ES']
+
+lang = os.environ.get('LANG', 'en_US')
+lang = lang.split('.')[0]
+
+translation = gettext.translation('base', LOCALE_DIR, languages=[lang], fallback=True)
+_ = translation.gettext
+
+
 def wash(input_dict: dict) -> None:
     """
     >>> wash({})
@@ -18,14 +31,14 @@ def wash(input_dict: dict) -> None:
                 if input_dict['dirty'] != True:
                     pass
                 else:
-                    print('wash your dirty dict')
+                    print(_('wash your dirty dict'))
             else:
-                print('wash your dict')
+                print(_('wash your dict'))
         except TypeError:
-            print('skill issue')
+            print(_('skill issue'))
 
     else:
-        print("can't wash your dict")
+        print(_("can't wash your dict"))
 
 
 if __name__ == '__main__':
