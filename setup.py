cc39c5aa2d30758d2150b28d74875042c0aa8a30
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-04 12:53:00 -0500

    Add link to code

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='dict_wash',
     description='wash your dicts',
+    project_urls={
+        "Source Code": "https://github.com/Hacksore/wash",
+    },
     long_description='takes in a dict and returns a clean dict',
     version='0.69.0',
     install_requires=[],
