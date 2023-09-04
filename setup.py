e00dc7943ca2a7473c87259585c05a5f25080cb3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-04 12:43:11 -0500

    Update desc

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='dict_wash',
+    description='wash your dicts',
+    long_description='takes in a dict and returns a clean dict',
     version='0.69.0',
     install_requires=[],
     packages=find_packages(),
