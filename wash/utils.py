9a2a53c17989346144936c26936c99e99ef953e1
Juan Valencia <juan7732@gmail.com>
2023-09-04 12:24:40 -0500

    Unit tests (#2)

--- wash/utils.py
+++ wash/utils.py
@@ -1,15 +1,36 @@
-def wash(input_dict):
-    if input_dict is not None:
+def wash(input_dict: dict) -> None: 
+    """
+    >>> wash({})
+    wash your dict
+    >>> wash({'dirty': True})
+    wash your dirty dict
+    >>> wash(None)
+    get a dict
+    can't wash your dict
+    """
+    
+    
+    if input_dict is None:
         print('get a dict')
 
     if isinstance(input_dict, dict):
         try:
-            if input_dict['dirty'] != True:
-                pass
+            if 'dirty' in input_dict:
+                if input_dict['dirty'] != True:
+                    pass
+                else:
+                    print('wash your dirty dict')
             else:
                 print('wash your dict')
-        finally:
-            print("wash your dict")
-
+        except TypeError:
+            print('skill issue')
+        
+    
     else:
         print("can't wash your dict")
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
